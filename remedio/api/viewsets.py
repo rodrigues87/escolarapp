5f6d39a42f425cc9064b7dfa63a6c968ad203871
pedro <pedroh.mix@gmail.com>
2020-10-28 17:19:59 -0300

    Adicionando tarefas

--- remedio/api/viewsets.py
+++ remedio/api/viewsets.py
@@ -7,3 +7,6 @@ from remedio.models import Remedio
 class RemedioViewSet(ModelViewSet):
     queryset = Remedio.objects.all()
     serializer_class = RemedioSerializer
+
+    def get_queryset(self):
+        return Remedio.objects.filter(usuario=self.request.user)
--- remedio/migrations/0002_auto_20201028_1713.py
+++ remedio/migrations/0002_auto_20201028_1713.py
@@ -26,7 +26,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='remedio',
             name='usuario',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(default=71, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
     ]
