87bd7cf8bb0da34e8a040632f869acca494cacf6
pedro.rodrigues <pedroh.mix@gmail.com>
2020-11-05 18:28:59 -0300

    Adicionando novas rotas no app

--- agendamento/api/viewsets.py
+++ agendamento/api/viewsets.py
@@ -7,3 +7,14 @@ from agendamento.api.serializers import AgendamentoSerializer
 class AgendamentoViewSet(ModelViewSet):
     queryset = Agendamento.objects.all()
     serializer_class = AgendamentoSerializer
+
+    def get_queryset(self):
+        return Agendamento.objects.filter(usuario__isnull=True)
+
+
+class MeusAgendamentoViewSet(ModelViewSet):
+    queryset = Agendamento.objects.all()
+    serializer_class = AgendamentoSerializer
+
+    def get_queryset(self):
+        return Agendamento.objects.filter(usuario=self.request.user)
--- agendamento/models.py
+++ agendamento/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+from usuarios.models import User
+
 
 class Agendamento(models.Model):
     nome_agendamento = models.CharField(max_length=40)
     data = models.DateField()
     horainicio = models.TimeField()
     horafinal = models.TimeField()
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE,null=True,blank=True)
 
     def __str__(self):
         return self.nome_agendamento
