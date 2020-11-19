ca4134cdc81865c5ef5229a06e69ae38f19e8c20
pedro <pedroh.mix@gmail.com>
2020-11-19 01:41:11 -0300

    Alterando api de impessoras e chamados

--- remedio/migrations/0018_auto_20201119_0041.py
+++ remedio/migrations/0018_auto_20201119_0041.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('remedio', '0017_auto_20201119_0034'),
+        ('remedio', '0016_recomendacao_quantidade_tomada_comprimidos'),
     ]
 
     operations = [
