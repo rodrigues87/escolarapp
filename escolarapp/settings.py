21a76501cba7a5ea03cfef05bd05c484c3231a8f
PEDRO <pedroh.mix@gmail.com>
2021-02-11 00:54:59 -0300

    Adicionando entrada para cors

--- escolarapp/settings.py
+++ escolarapp/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'remedio',
     'tarefas',
     'pokemon'
+    'corsheaders',
 
 ]
 
@@ -62,6 +63,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
